2d24a95693899a1b386b6bbc2038c56bde68dffa
maksymivashchenko <mivashchenko@react.vslabs.com>
2023-08-09 20:32:06 +0200

    Footer styles update

--- src/blocks/Footer/index.ts
+++ src/blocks/Footer/index.ts
@@ -4,18 +4,25 @@ import { styled } from '@mui/material/styles';
 const FooterWrapper = styled(Box)(({ theme }) => ({
   padding: '24px 16px',
   background: '#59585e',
-  minHeight: 80,
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
-  [theme.breakpoints.down('md')]: {
-    display: 'block',
-    textAlign: 'center'
+  [theme.breakpoints.down('sm')]: {
+    padding: '8px 16px'
   }
 }));
 
-const IconButton = styled(MuiIconButton)(() => ({
-  margin: '0 16px'
+const IconButton = styled(MuiIconButton)(({ theme }) => ({
+  margin: '0 16px',
+
+  [theme.breakpoints.down('sm')]: {
+    margin: 0,
+
+    '& svg': {
+      with: 18,
+      height: 18
+    }
+  }
 }));
 
 const Footer = {
