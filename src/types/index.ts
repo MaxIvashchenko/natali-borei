db7b95e0bedbc915e757952655bab2183e48f28a
Maksym Ivashchenko <maksym.iv@team.anyforsoft.com>
2024-03-10 13:07:54 +0000

    add LangDetails to export file in type folder

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,6 @@ export type { ItemFormProps } from './admin.page';
 export { CategoryIdName } from './admin.page';
 export type { IconTypes, IPaths, ISnackbarState, IUserDetails } from './common';
 export { LangEnum } from './common';
-export type { ICategory, ShopItem } from './content';
+export type { ICategory, LangDetails, ShopItem } from './content';
 export { CategoryType } from './content';
 export type { IRequestFormState } from './form';
