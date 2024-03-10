7ca938e2e36a83873410918ff664cedbe603d059
Maksym Ivashchenko <maksym.iv@team.anyforsoft.com>
2024-03-10 12:03:13 +0000

    Updated fav icon badge qty

--- src/componets/Header/components/FavoriteIcon/FavoriteIcon.tsx
+++ src/componets/Header/components/FavoriteIcon/FavoriteIcon.tsx
@@ -1,5 +1,5 @@
 import React, { memo } from 'react';
-import { Badge, IconButton } from '@mui/material';
+import { Badge, IconButton, Typography } from '@mui/material';
 import { IconComponent } from '@src/componets/Common';
 import { GLOBAL_COLORS } from '@src/constants';
 
@@ -14,14 +14,13 @@ const FavoriteIcon = ({
   isActive = false,
   qty = 0
 }: FavoriteIconProps) => (
-  <Badge badgeContent={qty} color='primary'>
-    <IconButton onClick={onClick}>
-      <IconComponent
-        name={isActive ? 'favoriteFill' : 'favorite'}
-        fill={GLOBAL_COLORS.gold}
-      />
-    </IconButton>
-  </Badge>
+  <IconButton onClick={onClick} style={{ position: 'relative' }}>
+    <IconComponent
+      name={isActive ? 'favoriteFill' : 'favorite'}
+      fill={GLOBAL_COLORS.gold}
+    />
+    {Boolean(qty) && <Typography color='white' pb={0.5} pl={0.2} style={{ position: 'absolute' }}>{qty}</Typography>}
+  </IconButton>
 );
 
 export default memo(FavoriteIcon);
