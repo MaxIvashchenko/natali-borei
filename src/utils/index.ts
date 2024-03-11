c7ed5a5014e444ee421b16f0f936b2ec51a10316
Maksym Ivashchenko <maksym.iv@team.anyforsoft.com>
2024-03-11 20:33:38 +0000

    updated export path

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,4 @@
 export * from './emailFormatText';
-export * from './getImageUrl';
 export { getTelegramURL } from './getTelegramURL';
 export { default as imgLoader } from './imgLoader';
 export * from './redirect';
